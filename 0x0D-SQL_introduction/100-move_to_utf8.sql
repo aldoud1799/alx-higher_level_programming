c7438380cc46887c9e3ab509297e8412337a6c34
Abdullah Abbas <abdallah.abbas.jr@gmail.com>
2023-03-14 15:18:32 -0700

    102

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,2 +1,4 @@
--- displays the average temperature by city ordered by temperature
-SELECT city, AVG(value) as avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
+SELECT t.city, AVG(t.value) AS avg_temp -- Calculate average temperature by city
+FROM temperatures t
+GROUP BY t.city -- Group by city
+ORDER BY MAX(t.value) DESC; -- Order by highest temperature first
