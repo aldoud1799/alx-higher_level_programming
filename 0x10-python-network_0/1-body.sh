319042191d8ae6722afc7ad9274efe446bfd18bc
aldoud1799 <adallah.abbas.jr@gmail.com>
2023-04-27 04:46:07 -0700

    3st

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Takes in a URL, sends a GET request to the URL, displays the body of the response
-curl -sX GET "$1" -L 200
-
+# send a GET request to an URL with curl, and display the body of the response
+curl -sL "$1"
